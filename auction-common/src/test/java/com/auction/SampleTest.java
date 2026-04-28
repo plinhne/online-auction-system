da06f88758965baf165a31bb69147f46af488e91
thnguyen5927 <25023345@vnu.edu.vn>
2026-04-28 23:10:20 +0700

    test: add sample junit test

---
+++ auction-common/src/test/java/com/auction/SampleTest.java
@@ -0,0 +1,12 @@
+package com.auction;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    void testExample() {
+        assertEquals(2, 1 + 1);
+    }
+}
