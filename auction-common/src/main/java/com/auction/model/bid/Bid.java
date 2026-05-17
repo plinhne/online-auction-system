3d74b69c391ba913369f9ffaa20d997f504db57e
Tran Nguyen Khanh Linh <25023302@vnu.edu.vn>
2026-05-17 22:58:18 +0700

    feat: add auctionID, placeAt(final, LocalDateTime), and their getters for Bid

--- auction-common/src/main/java/com/auction/model/bid/Bid.java
+++ auction-common/src/main/java/com/auction/model/bid/Bid.java
@@ -1,21 +1,28 @@
 package com.auction.model.bid;
 
 import com.auction.model.user.User;
+import java.time.LocalDateTime;
 
 public class Bid {
     private int id;
     private double amount;
     private BidStatus status;
     private User bidder;
+    private final int auctionId; // Gắn liền với một phiên đấu giá tồn tại
+    private final LocalDateTime placedAt; // Thời gian đặt bid
 
-    public Bid(int id, double amount, User bidder) {
+    public Bid(int id, double amount, User bidder, int auctionId) {
         this.id = id;
         this.amount = amount;
         this.bidder = bidder;
         this.status = BidStatus.PENDING;
+        this.auctionId = auctionId;
+        this.placedAt = LocalDateTime.now(); // Tự động lấy thời gian hiện tại khi tạo bid
     }
-
-    public double getAmount() {
-        return amount;
-    }
+//getters
+    public int getId() { return id; }
+    public int getAuctionId() { return auctionId; }
+    public double getAmount() { return amount; }
+    public User getBidder() { return bidder; }
+    public LocalDateTime getPlacedAt() { return placedAt; }
 }
